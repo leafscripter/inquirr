e56d83125392b8042b0c8fa43c1ac0293c62ff1f
Qaysar Manken <qaysarmanken@kaisar.local>
2023-10-07 12:38:00 +0600

    added new attributes to user schema

--- models/user.js
+++ models/user.js
@@ -2,12 +2,15 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 userSchema = new Schema( {
-	
 	unique_id: Number,
 	email: String,
+	country: String, 
+	language: String,
+	description: String,
+	verified: Boolean,
 	username: String,
 	password: String,
-	passwordConf: String
+	passwordConf: String,
 }),
 User = mongoose.model('User', userSchema);
 
