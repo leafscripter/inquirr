a405b221377038962cf5187efa9c9ab43532835f
Annassir <annassirmus@gmail.com>
2023-10-07 13:43:01 +0600

    password encryption in registration functionality

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var bcrypt = require('bcrypt');
+
 var router = express.Router();
 var User = require('../models/user');
 
@@ -8,77 +10,79 @@ router.get('/', function (req, res, next) {
 
 
 router.post('/', function(req, res, next) {
-	console.log(req.body);
-	var personInfo = req.body;
+    console.log(req.body);
+    var personInfo = req.body;
 
+    if (!personInfo.email || !personInfo.username || !personInfo.password || !personInfo.passwordConf) {
+        res.send();
+    } else {
+        if (personInfo.password == personInfo.passwordConf) {
+            // Hash and salt the password
+            bcrypt.hash(personInfo.password, 10, function(err, hashedPassword) {
+                if (err) {
+                    console.log(err);
+                    return next(err);
+                }
 
-	if(!personInfo.email || !personInfo.username || !personInfo.password || !personInfo.passwordConf){
-		res.send();
-	} else {
-		if (personInfo.password == personInfo.passwordConf) {
+                // Store the hashed password in the database
+                User.findOne({ email: personInfo.email }, function(err, data) {
+                    if (!data) {
+                        var c;
+                        User.findOne({}, function(err, data) {
+                            if (data) {
+                                console.log("if");
+                                c = data.unique_id + 1;
+                            } else {
+                                c = 1;
+                            }
 
-			User.findOne({email:personInfo.email},function(err,data){
-				if(!data){
-					var c;
-					User.findOne({},function(err,data){
+                            var newPerson = new User({
+                                unique_id: c,
+                                email: personInfo.email,
+                                username: personInfo.username,
+                                password: hashedPassword, // Store the hashed password
+                                passwordConf: personInfo.passwordConf
+                            });
 
-						if (data) {
-							console.log("if");
-							c = data.unique_id + 1;
-						}else{
-							c=1;
-						}
+                            newPerson.save(function(err, Person) {
+                                if (err)
+                                    console.log(err);
+                                else
+                                    console.log('Success');
+                            });
 
-						var newPerson = new User({
-							unique_id:c,
-							email:personInfo.email,
-							username: personInfo.username,
-							password: personInfo.password,
-							passwordConf: personInfo.passwordConf
-						});
-
-						newPerson.save(function(err, Person){
-							if(err)
-								console.log(err);
-							else
-								console.log('Success');
-						});
-
-					}).sort({_id: -1}).limit(1);
-					res.send({"Success":"You are regestered,You can login now."});
-				}else{
-					res.send({"Success":"Email is already used."});
-				}
-
-			});
-		}else{
-			res.send({"Success":"password is not matched"});
-		}
-	}
+                        }).sort({ _id: -1 }).limit(1);
+                        res.send({ "Success": "You are registered, you can login now." });
+                    } else {
+                        res.send({ "Success": "Email is already used." });
+                    }
+                });
+            });
+        } else {
+            res.send({ "Success": "Password is not matched" });
+        }
+    }
 });
 
 router.get('/login', function (req, res, next) {
 	return res.render('login.ejs');
 });
 
-router.post('/login', function (req, res, next) {
-	//console.log(req.body);
-	User.findOne({email:req.body.email},function(err,data){
-		if(data){
-			
-			if(data.password==req.body.password){
-				//console.log("Done Login");
-				req.session.userId = data.unique_id;
-				//console.log(req.session.userId);
-				res.send({"Success":"Success!"});
-				
-			}else{
-				res.send({"Success":"Wrong password!"});
-			}
-		}else{
-			res.send({"Success":"This Email Is not regestered!"});
-		}
-	});
+router.post('/login', function(req, res, next) {
+    User.findOne({ email: req.body.email }, function(err, data) {
+        if (data) {
+            bcrypt.compare(req.body.password, data.password, function(err, result) {
+                if (result === true) {
+                    req.session.userId = data.unique_id;
+                    res.send({ "Success": "Success!" });
+                } else {
+                    res.send({ "Success": "Wrong password!" });
+                }
+            });
+        } else {
+            res.send({ "Success": "This Email Is not registered!" });
+        }
+    });
 });
 
 router.get('/profile', function (req, res, next) {
@@ -90,7 +94,7 @@ router.get('/profile', function (req, res, next) {
 			res.redirect('/');
 		}else{
 			//console.log("found");
-			return res.render('data.ejs', {"name":data.username,"email":data.email});
+			return res.render('user.ejs', {"name":data.username,"email":data.email});
 		}
 	});
 });
